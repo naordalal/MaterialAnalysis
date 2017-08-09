2d104bd6863c4523c15a393be2dc1a3eb2bd7af9
naordalal <naordal@post.bgu.ac.il>
2017-08-09 16:06:37 +0300

    first commit

---
+++ MapAnalyzer/src/MainPackage/Main.java
@@ -0,0 +1,30 @@
+package MainPackage;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		String fileName = "C:/Users/naordalal/Desktop/shipment_0.txt";
+		 try {
+			for (String line : Files.readAllLines(Paths.get(fileName),Charset.forName("IBM862")))
+			 {
+				 System.out.println(line.split("\\|").length);
+				List<String> x = Arrays.asList(line.split("\\|")).stream().map(s -> s.trim()).filter(s->!s.equals("")).collect(Collectors.toList());
+				 x.stream().forEach(s -> System.out.print(s + "|"));
+			 }
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
